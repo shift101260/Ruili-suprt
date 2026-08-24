ffc3a14d4c9d1a23dbf11efa18066ec62ba8c034
shift101260 <shift101260@gmail.com>
2026-08-24 18:42:49 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,7 @@
+// js/config.js
 export const SUPABASE_URL = 'https://cksoeikllbzdihlesraa.supabase.co';
 export const SUPABASE_KEY = 'sb_publishable_V9QZLQBKv2SGrthA814LAg_x167CTRD';
 
 // 建立並匯出 Supabase Client 實例
 export const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+export const STORAGE_KEY = 'RUILI_SYSTEM_CASES_2026';
