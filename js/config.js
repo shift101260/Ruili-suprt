dd041715dde1e780ad1a5fda1e825772b1463eb5
shift101260 <shift101260@gmail.com>
2026-08-24 18:31:19 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
+export const SUPABASE_URL = 'https://cksoeikllbzdihlesraa.supabase.co';
+export const SUPABASE_KEY = 'sb_publishable_V9QZLQBKv2SGrthA814LAg_x167CTRD';
 
+// 建立並匯出 Supabase Client 實例
+export const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
