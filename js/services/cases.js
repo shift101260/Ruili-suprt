26d1706fe94d8d7e89871c2d7f319f956429fa58
shift101260 <shift101260@gmail.com>
2026-08-24 18:31:52 +0800

    Update cases.js

--- js/services/cases.js
+++ js/services/cases.js
@@ -1,1 +1,22 @@
+import { supabaseClient } from '../config.js';
 
+export async function saveCase(content) {
+  if (!content) throw new Error('請輸入內容');
+
+  const { data, error } = await supabaseClient
+    .from('cases')
+    .insert([{ data: { content } }]);
+
+  if (error) throw new Error(error.message);
+  return data;
+}
+
+export async function loadCases() {
+  const { data, error } = await supabaseClient
+    .from('cases')
+    .select('*')
+    .order('id', { ascending: false });
+
+  if (error) throw new Error(error.message);
+  return data;
+}
