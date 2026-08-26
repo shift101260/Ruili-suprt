d8c9f134c0c4343e9283bb133c855383e27df6c3
shift101260 <shift101260@gmail.com>
2026-08-26 16:13:42 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 export const SUPABASE_URL = 'https://cksoeikllbzdihlesraa.supabase.co';
-export const SUPABASE_KEY = 'sb_publishable_V9QZLQBkv2SGrthA814LAg_x167CTRD';
+export const SUPABASE_KEY = 'sb_secret_S2gIMazlgsJP-mMg_JUH1Q_fbcAdE8g';
 export const STORAGE_KEY = 'RUILI_SYSTEM_CASES_2026';
 
 // 使用 index.html 載入的全域 Supabase 套件建立連線
